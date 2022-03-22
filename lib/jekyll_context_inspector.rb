155308b6ee771adf1aaa3955c7281e46cd65da15
Mike Slinn <mslinn@mslinn.com>
2022-03-22 17:37:23 -0400

    -

--- lib/jekyll_context_inspector.rb
+++ lib/jekyll_context_inspector.rb
@@ -8,41 +8,38 @@ module JekyllPlubinContextInspectorName
   PLUGIN_NAME = "context_inspector"
 end
 
-module Jekyll
+class ContextInspector < Liquid::Tag
+  def render(context)
+    site = context.registers[:site]
+    inspector_enabled = site.config["context_inspector"]
+    return if inspector_enabled.nil? || !inspector_enabled
 
-  class ContextInspector < Liquid::Tag
-    def render(context)
-      site = context.registers[:site]
-      inspector_enabled = site.config["context_inspector"]
-      return if inspector_enabled.nil? || !inspector_enabled
+    mode = site.config["env"]["JEKYLL_ENV"]
+    return unless inspector_enabled == "force" || mode == "development"
 
-      mode = site.config["env"]["JEKYLL_ENV"]
-      return unless inspector_enabled == "force" || mode == "development"
-
-      dump_info(context)
-    end
-
-    private
-
-    def dump_info(context)
-      page = context.registers[:page]
-      info do
-        key_value_pairs = context.registers.map do |key, value|
-          "  <code>#{key}</code> has a value with type <code>#{value.class}</code>"
-        end
-        vars = page.keys.sort.join("</code>, <code>")
-        <<~END_MESSAGE
-          context for #{page.path} is of type #{context.class}.
-          context.registers for #{page.path} contains the following key/value pairs:
-          #{key_value_pairs.join("\n")}
-          #{PLUGIN_NAME}: #{page.path} contains the following key/value pairs:
-          <p class='info'>Jekyll variables for this page are:
-          <code>#{vars}</code></p>
-        END_MESSAGE
-      end
-    end
+    dump_info(context)
   end
 
-  Liquid::Template.register_tag(JekyllPlubinContextInspectorName::PLUGIN_NAME, ContextInspector)
-  info { "Loaded #{JekyllPlubinContextInspectorName::PLUGIN_NAME} v#{JekyllContextInspector::VERSION} plugin." }
+  private
+
+  def dump_info(context)
+    page = context.registers[:page]
+    info do
+      key_value_pairs = context.registers.map do |key, value|
+        "  <code>#{key}</code> has a value with type <code>#{value.class}</code>"
+      end
+      vars = page.keys.sort.join("</code>, <code>")
+      <<~END_MESSAGE
+        context for #{page.path} is of type #{context.class}.
+        context.registers for #{page.path} contains the following key/value pairs:
+        #{key_value_pairs.join("\n")}
+        #{JekyllPlubinContextInspectorName::PLUGIN_NAME}: #{page.path} contains the following key/value pairs:
+        <p class='info'>Jekyll variables for this page are:
+        <code>#{vars}</code></p>
+      END_MESSAGE
+    end
+  end
 end
+
+Liquid::Template.register_tag(JekyllPlubinContextInspectorName::PLUGIN_NAME, ContextInspector)
+PluginMetaLogger.instance.info { "Loaded #{JekyllPlubinContextInspectorName::PLUGIN_NAME} v#{JekyllContextInspector::VERSION} plugin." }
