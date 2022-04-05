5e0b4c1ca8b7a9fa48b41285a0f6cc8d0ae1f97d
Mike Slinn <mslinn@micronauticsresearch.com>
2022-04-05 17:46:17 -0400

    -

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = "random"
+
+  # See https://relishapp.com/rspec/rspec-core/docs/command-line/only-failures
+  config.example_status_persistence_file_path = "spec/status_persistence.txt"
 end
